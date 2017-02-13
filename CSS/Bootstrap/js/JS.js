12e7c2f47a3b7ab09005b38f8fe3de0ec6f824ae
Omar <omarmandeeli@gmail.com>
2017-02-14 04:19:53 +0800

    2.14

---
+++ CSS/Bootstrap/js/JS.js
@@ -0,0 +1,24 @@
+var $document = $(document),
+    $element = $('.navbar'),
+    navbarDefault = 'navbar-default';
+    navbarTransparent = 'navbar-transparent';
+
+    fadeInDown = 'fadeInDown';
+
+
+$document.scroll(function(){
+  if ($document.scrollTop() >= 10 ) {
+    $element.addClass(navbarDefault);
+    $element.removeClass(navbarTransparent);
+
+    $element.addClass(fadeInDown);
+} else {
+
+    $element.addClass(navbarTransparent);
+    $element.removeClass(navbarDefault);
+
+
+    $element.removeClass(fadeInDown);
+}
+});
+
